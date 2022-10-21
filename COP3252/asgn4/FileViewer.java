dcace5e1985cd7b898478e602cbb508d7d3f7627
jacksonmcafee <jacksonmcafee1@gmail.com>
2022-10-21 01:37:47 -0400

    java asgn 4 done

--- COP3252/asgn4/FileViewer.java
+++ COP3252/asgn4/FileViewer.java
@@ -2,17 +2,26 @@ import java.io.*;
 import java.util.*;
 import java.nio.file.*;
 
-// Jackson McAfee, COP3252 Adv. Java Prog. Asgn 4
+/* 
+Jackson McAfee, COP3252 Adv. Java Prog. Asgn 4
 
-// I am a huge fan of the ternary operator and might have overused it here. Oops!
+Did not waste a bunch of time on a file parsing function this time around.
+Served me well.
+
+I organized my program by int selections:
+	1 is info, 2 is view, 3 is copy, 4 is compare
+
+I don't know if I should've caught exceptions more specifically than just
+the Exception class, but it did the job. I was having problems with 
+IOException/FileNotFoundException not catching so I just defaulted to Exception. 
+*/
 
 public class FileViewer {
 	public static void main(String[] args) throws Exception {
 
-		int selection = 1; 		 // 1 is info, 2 is view, 3 is copy, 4 is compare
+		int selection = 1; 		 
 		String fileParam1 = " "; // for all use cases
 		String fileParam2 = " "; // for copy and compare case
-		boolean maybeDirectory = false;
 
 		if(args.length == 0) {
 			fileParam1 = ".";
@@ -21,7 +30,6 @@ public class FileViewer {
 			String flag = args[0];     // makes code more readable
 			if (flag.matches("-i")) {
 				fileParam1 = ".";
-				maybeDirectory = true;
 			}
 			else {
 				fileParam1 = args[0]; // file passed 
@@ -68,116 +76,135 @@ public class FileViewer {
 			PrintError("");
 		}
 
-		if (selection == 1) {
-			// "-i" or nothing was passed
-			File f = new File(fileParam1);
-			long size = 0;
-
-			if (f.isFile()) {
-				// prints filepath 
-				System.out.printf("File Path: %s %n", f.getAbsolutePath());
-				
-				// prints executable status
-				String executable = (f.canExecute() ? "True" : "False");
-				System.out.printf("Is executable?: %s %n", executable);
-
-				// prints size of file
-				System.out.printf("Size: %d bytes %n", f.length());
-				
-				// prints out date and time 
-				Date date = new Date(f.lastModified());
-				System.out.printf("Last Modified Date: %tc %n", date);
-			}
-			else if (f.isDirectory()) {
-				System.out.printf("%-10s %s %n", "Size", "Filename");
-				File[] files = f.listFiles();
-
-				for (File x : files) {
-					// get filesize, if directory, set to *
-					size = x.length();
-					String name = (x.isDirectory() ? x.getName() + "*" : x.getName());
-
-					System.out.printf("%-10d %s %n", size, name);
-				}
-			}
-			else {
-				// not valid file, not valid directory, 
-				PrintError("Invalid file or directory passed.");
-			}
-		}
-		else if (selection == 2) {
-			// "-v" was passed
-			try {
+		switch (selection) {
+			case 1: {
+				// "-i" or nothing was passed
 				File f = new File(fileParam1);
-				if(f.isFile()){  // if the file passed exists and is a file, 
-					Scanner s = new Scanner(f);
-					while(s.hasNextLine())
-						System.out.println(s.nextLine()); // prints out each line while there are still lines to print
-					s.close();
+				long size = 0;
+
+				if (f.isFile()) {
+					// prints filepath 
+					System.out.printf("File Path: %s %n", f.getAbsolutePath());
+					
+					// prints executable status
+					String executable = (f.canExecute() ? "true" : "false");
+					System.out.printf("Is executable?: %s %n", executable);
+
+					// prints size of file
+					System.out.printf("Size: %d bytes %n", f.length());
+					
+					// prints out date and time
+					Date date = new Date(f.lastModified());
+					System.out.printf("Last Modified Date: %tc %n", date);
 				}
+				else if (f.isDirectory()) {
+					System.out.printf("%-10s %s %n", "Size", "Filename");
+					File[] files = f.listFiles();
+
+					for (File x : files) {
+						// get filesize, if directory, set to *
+						size = x.length();
+						String name = (x.isDirectory() ? x.getName() + "*" : x.getName());
+
+						System.out.printf("%-10d %s %n", size, name);
+					}
+				}
+				else {
+					// not valid file, not valid directory, 
+					PrintError("Invalid file or directory passed.");
+				}
+				break;
 			}
-			catch (Exception e) {
-				PrintError("File not found.");
-			}	
-		}
-		else if (selection == 3) {
-			// "-c" was passed
+
+			case 2: {
+				// "-v" was passed
+				try {
+					File f = new File(fileParam1);
+					// if the file passed exists and is a file
+					if(f.isFile()){  
+						Scanner s = new Scanner(f);
+						while(s.hasNextLine())
+							// outputs lines until eof
+							System.out.println(s.nextLine()); 
+						s.close();
+					}
+				}
+				catch (Exception e) {
+					PrintError("File not found.");
+				}
+				break;
+			}
+			
+			case 3: {
+				// "-c" was passed
+				if (fileParam1.matches(fileParam2)) {
+					PrintError("Cannot copy file to itself.");
+				}
+
+				try {
+					// try to create files based on param1/param2
+					File f1 = new File(fileParam1);
+					File f2 = new File(fileParam2); 
+
+					if (f2.isFile()) {
+						System.out.printf("File %s already exists.", fileParam2);
+						PrintError("");
+					}
+
+					// create in/out streams based on files
+					FileInputStream in = new FileInputStream(f1);
+					FileOutputStream out = new FileOutputStream(f2);
+
+					int i;
+					while ((i = in.read()) != -1) {
+						// write each byte from instream to outstream
+						out.write(i);
+					}
+
+					if (in != null) {
+						in.close(); // close instream
+					}
+					if (out != null) {
+						out.close(); // close outstream
+					}
+				}
+				catch (Exception e) {
+					// if something throws, catch here
+					PrintError("File not found.");
+				}	
+				System.out.printf("File %s was copied to file %s successfully!", fileParam1, fileParam2);
+				break;
+			}
+
+			case 4: {
+			// if same file, return immediately
 			if (fileParam1.matches(fileParam2)) {
-				PrintError("Cannot copy file to itself.");
+				System.out.printf("The two files %s and %s are the same. %n", fileParam1, fileParam2);
+				return;
 			}
 
-			try {
-				// try to create files based on param1/param2
-				File f1 = new File(fileParam1);
-				File f2 = new File(fileParam2); 
-
-				if (f2.isFile()) {
-					PrintError("That file already exists");
-				}
-
-				// create in/out streams based on files
-				FileInputStream in = new FileInputStream(f1);
-				FileOutputStream out = new FileOutputStream(f2);
-
-				int i;
-	            while ((i = in.read()) != -1) {
-	            	// write each byte from instream to outstream
-	                out.write(i);
-            	}
-
-            	if (in != null) {
-                	in.close(); // close instream
-            	}
-            	if (out != null) {
-               		out.close(); // close outstream
-            	}
-			}
-			catch (Exception e) {
-				// if something throws, catch here
-				PrintError("File not found.");
-			}	
-       		System.out.println("File was copied successfully!");
-    	}
-		else {
 			// selection == 4, "-d" was passed
 			try {
+				// create paths from fileParams b/c Files.nBR requires path objects
 				Path path1 = FileSystems.getDefault().getPath(fileParam1);
 				Path path2 = FileSystems.getDefault().getPath(fileParam2);
 
-				BufferedReader bf1 = Files.newBufferedReader(path1);
-         		BufferedReader bf2 = Files.newBufferedReader(path2);
+				// create BRs from paths
+				BufferedReader br1 = Files.newBufferedReader(path1);
+         		BufferedReader br2 = Files.newBufferedReader(path2);
 
-         		long lineNumber = 1;
-	        	String line1 = "", line2 = "";
-	        	while ((line1 = bf1.readLine()) != null) {
-	            	line2 = bf2.readLine();
-	            	if (line2 == null || !line1.equals(line2)) {
+	        	String l1 = "", l2 = "";
+				// get each line from each BR, compare with matches
+				// if diff is found, print and return
+	        	while ((l1 = br1.readLine()) != null) {
+	            	l2 = br2.readLine();
+	            	if (l2 == null || !l1.matches(l2)) {
 	                	System.out.printf("The two files %s and %s are not the same. %n", fileParam1, fileParam2);
 	                	return;
 	            	}
-	            	lineNumber++;
 	        	}
-	        	if (bf2.readLine() == null) {
+				// if no more lines at same time, print and return
+	        	if (br2.readLine() == null) {
 	               	System.out.printf("The two files %s and %s are the same. %n", fileParam1, fileParam2);
 	        	}
 	        	else {
@@ -186,9 +213,12 @@ public class FileViewer {
         	}
         	catch (Exception e) {
         		PrintError("File not found.");
-        	}	
-    	}
+			}
+			break;
+			}
+		}
 	}
+	// handles all errors as they're found
 	private static void PrintError(String s) {
 		System.err.println(s);
 		System.err.printf("Usage: java -jar hw4.jar [-i [<file>|<directory>]|-v <file>|-c <sourceFile> <destFile>|-d <file1> <file2>]\n");
