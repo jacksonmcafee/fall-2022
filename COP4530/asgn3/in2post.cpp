ce138ac8d3890cdb10d73e856ccfee53bcb3f9bd
jacksonmcafee <jacksonmcafee1@gmail.com>
2022-10-21 00:46:02 -0400

    listen to macroblank on youtube

--- COP4530/asgn3/in2post.cpp
+++ COP4530/asgn3/in2post.cpp
@@ -5,22 +5,40 @@
 using namespace cop4530;
 
 int precedence(char c);
-void inToPost(std::string s);
+std::string inToPost(std::string s);
+
+int evaluateExpression(std::string s);
+bool balancedParens(std::string s);
+int isOperator(char c);
+int isInteger(char c);
+int operation(int a, int b, char op);
 
 int main() {
+  // declare input string and get it from user
   std::string input;
-
   std::cout << "Enter your expression: ";
   std::getline(std::cin, input);
 
-  inToPost(input);
+  if (!balancedParens(input)) {
+    std::cout << "Unbalanced parentheses, this expression cannot be solved."
+              << std::endl;
+    return 1;
+  }
+
+  // overwrite input as postfix notation
+  input = inToPost(input);
+
+  // pass input to evalExpress and return solved value
+  int val = evaluateExpression(input);
+
+  // print expression
+  std::cout << input << " = " << val << std::endl;
+
   return 0;
 }
 
-int prec(char c) {
-  if (c == '^')
-    return 3;
-  else if (c == '/' || c == '*')
+int precedence(char c) {
+  if (c == '/' || c == '*')
     return 2;
   else if (c == '+' || c == '-')
     return 1;
@@ -28,35 +46,135 @@ int prec(char c) {
     return -1;
 }
 
-void inToPost(std::string s) {
-  Stack<char> _stack;
-  std::string return_string;
+std::string inToPost(std::string s) {
 
-  for (int i = 0; i < s.length(), i++) {
+  Stack<char> stack;
+  std::string result;
+
+  for (int i = 0; i < s.length(); i++) {
     char c = s[i];
 
+    // If the scanned character is
+    // an operand, add it to output string.
     if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') ||
-        (c >= '0' && c <= '9')) {
-      return_string += c;
-    } else if (c == '(') {
-      _stack.push(c);
-    } else if (c == ')') {
-      while (_stack.top() != '(') {
-        return_string += _stack.top();
-        _stack.pop();
+        (c >= '0' && c <= '9'))
+      result += c;
+
+    // If the scanned character is an
+    // ‘(‘, push it to the stack.
+    else if (c == '(')
+      stack.push('(');
+
+    // If the scanned character is an ‘)’,
+    // pop and to output string from the stack
+    // until an ‘(‘ is encountered.
+    else if (c == ')') {
+      while (stack.top() != '(') {
+        result += stack.top();
+        stack.pop();
       }
-      _stack.pop();
-    } else {
-      while (!_stack.empty() && precedence(s[i]) <= precedence(_stack.top())) {
-        return_string += _stack.top();
-        _stack.pop();
+      stack.pop();
+    }
+
+    // If an operator is scanned
+    else {
+      while (!stack.empty() && precedence(s[i]) <= precedence(stack.top())) {
+        result += stack.top();
+        stack.pop();
       }
+      stack.push(c);
     }
   }
-  while (!_stack.empty()) {
-    return_string += _stack.top();
-    _stack.pop();
+
+  // Pop all the remaining elements from the stack
+  while (!stack.empty()) {
+    result += stack.top();
+    stack.pop();
   }
 
-  std::cout << return_string << std::endl;
+  std::cout << result << std::endl;
+}
+
+bool balancedParens(std::string s) {
+  Stack<char> stack;
+  bool noParens = true;
+  int i = 0;
+
+  for (int i = 0; i < s.length(); i++) {
+    if (s[i] == '(') {
+      noParens = false;
+    }
+  }
+
+  if (noParens) {
+    return true;
+  }
+
+  if (s.size() == 0) {
+    return true;
+  }
+
+  while (i < s.size()) {
+
+    if (s[i] == '(') {
+      stack.push(s[i]);
+    }
+
+    else if ((s[i] == ')' && !stack.empty() && stack.top() == '(')) {
+      stack.pop();
+    } else {
+      return false;
+    }
+    i++;
+  }
+
+  if (stack.empty()) {
+    return true;
+  }
+  return false;
+}
+
+// takes postfix notation and determines an answer
+int evaluateExpression(std::string s) {
+  int a, b;
+  Stack<int> stack;
+  std::string::iterator it;
+  for (it = s.begin(); it != s.end(); it++) {
+    // read elements and perform postfix evaluation
+    if (isOperator(*it) != -1) {
+      a = stack.top();
+      stack.pop();
+      b = stack.top();
+      stack.pop();
+      stack.push(operation(a, b, *it));
+    } else if (isInteger(*it) > 0) {
+      stack.push(*it - 48);
+    }
+  }
+  return stack.top();
+}
+
+int isOperator(char c) {
+  if (c == '+' || c == '-' || c == '*' || c == '/' || c == '^')
+    return 1; // character is an operator
+  return -1;  // not an operator
+}
+
+int isInteger(char c) {
+  if (c >= '0' && c <= '9')
+    return 1; // character is an operand
+  return -1;  // not an operand
+}
+
+int operation(int a, int b, char op) {
+  // Perform operation
+  if (op == '+')
+    return b + a;
+  else if (op == '-')
+    return b - a;
+  else if (op == '*')
+    return b * a;
+  else if (op == '/')
+    return b / a;
+  return -1;
 }
