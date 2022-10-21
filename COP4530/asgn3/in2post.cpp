99820708a63854e0bccdd4995a0c5ff73dec7cd6
jacksonmcafee <jacksonmcafee1@gmail.com>
2022-10-20 22:41:45 -0400

    almost done w/ postfix

--- COP4530/asgn3/in2post.cpp
+++ COP4530/asgn3/in2post.cpp
@@ -1,3 +1,62 @@
 #include "Stack.h"
 #include <iostream>
+#include <sstream>
 
+using namespace cop4530;
+
+int precedence(char c);
+void inToPost(std::string s);
+
+int main() {
+  std::string input;
+
+  std::cout << "Enter your expression: ";
+  std::getline(std::cin, input);
+
+  inToPost(input);
+  return 0;
+}
+
+int prec(char c) {
+  if (c == '^')
+    return 3;
+  else if (c == '/' || c == '*')
+    return 2;
+  else if (c == '+' || c == '-')
+    return 1;
+  else
+    return -1;
+}
+
+void inToPost(std::string s) {
+  Stack<char> _stack;
+  std::string return_string;
+
+  for (int i = 0; i < s.length(), i++) {
+    char c = s[i];
+
+    if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') ||
+        (c >= '0' && c <= '9')) {
+      return_string += c;
+    } else if (c == '(') {
+      _stack.push(c);
+    } else if (c == ')') {
+      while (_stack.top() != '(') {
+        return_string += _stack.top();
+        _stack.pop();
+      }
+      _stack.pop();
+    } else {
+      while (!_stack.empty() && precedence(s[i]) <= precedence(_stack.top())) {
+        return_string += _stack.top();
+        _stack.pop();
+      }
+    }
+  }
+  while (!_stack.empty()) {
+    return_string += _stack.top();
+    _stack.pop();
+  }
+
+  std::cout << return_string << std::endl;
+}
