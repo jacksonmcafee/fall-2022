ef3b824ffc6fa7b94100fe206468d70a34dab9bb
jacksonmcafee <jacksonmcafee1@gmail.com>
2022-11-04 01:28:23 -0400

    asgn 5 done!!

--- COP4530/asgn5/BET.h
+++ COP4530/asgn5/BET.h
@@ -3,54 +3,51 @@
 
 #include <string>
 
-namespace cop4530 {
+template <typename T> class BET {
+public:
+  // declare & define BinaryNode struct
+  struct BinaryNode {
+    T element;
+    BinaryNode *left;
+    BinaryNode *right;
 
-    template <typename T>
-    class BET {
-        public:
+    // constructor
+    BinaryNode(const T &e = T{}, BinaryNode *l = nullptr,
+               BinaryNode *r = nullptr)
+        : element{e}, left{l}, right{r} {}
+  };
 
-            // declare & define BinaryNode struct
-            struct BinaryNode {
-                T element;
-                BinaryNode* left;
-                BinaryNode* right;
-                
-                // constructor
-                BinaryNode(const T& e =  T{}, BinaryNode *l = nullptr, BinaryNode *r = nullptr) : element{e}, left{l}, right{r} {}
-            };
+  // declare constructors and destructor
+  BET();
+  BET(const std::string &postfix);
+  BET(const BET &tree);
+  ~BET();
 
-            // declare constructors and destructor
-            BET();
-            BET(const string& postfix);
-            BET(const BET& tree);
-            ~BET();
+  // declare class functions
+  bool buildFromPostfix(const std::string &postfix);
+  const BET &operator=(const BET &);
+  void printInfixExpression();
+  void printPostfixExpression();
 
-            // declare class functions 
-            bool buildFromPostfix(const string& postfix);
-            const BET& operator=(const BET &);
-            void printInfixExpression();
-            void printPostfixExpression();
-            
-            // declare getter functions
-            size_t size();
-            size_t leaf_nodes();
-            bool empty();
+  // declare getter functions
+  size_t size();
+  size_t leaf_nodes();
+  bool empty();
 
-        private:
-            // declare helper functions
-            void printInfixExpression(BinaryNode *n);
-            void makeEmpty(BinaryNode* &t);
-            BinaryNode* clone(BinaryNode *t);
-            void printPostfixExpression(BinaryNode *n);
+private:
+  // declare helper functions
+  void printInfixExpression(BinaryNode *n);
+  void makeEmpty(BinaryNode *&t);
+  BinaryNode *clone(BinaryNode *t);
+  void printPostfixExpression(BinaryNode *n);
 
-            size_t size(BinaryNode *t);
-            size_t leaf_nodes(BinaryNode *t);
+  size_t size(BinaryNode *t);
+  size_t leaf_nodes(BinaryNode *t);
 
-            // stores pointer to tree root
-            BinaryNode* root;
-
-    }
-}
+  // stores pointer to tree root
+  BinaryNode *root;
+};
 
 #include "BET.hpp"
+
 #endif
--- COP4530/asgn5/BET.hpp
+++ COP4530/asgn5/BET.hpp
@@ -1,7 +1,13 @@
+#include <string>
+#include <iostream>
+#include <sstream>
 #include <stack>
-#include "Bet.h"
 
-using namespace cop4530;
+#include "BET.h"
+
+bool isOperator(std::string s);
+bool isOperand(std::string s);
+int precedence(std::string s);
 
 /* Constructors and Destructor */
 
@@ -13,7 +19,7 @@ BET<T>::BET() {
 
 template <typename T>
 // postfix constructor
-BET<T>::BET(const string& postfix) {
+BET<T>::BET(const std::string& postfix) {
     root = new BinaryNode;
     buildFromPostfix(postfix);
 }
@@ -35,25 +41,78 @@ BET<T>::~BET() {
 /* Class Functions */
 
 template <typename T>
-bool BET<T>::buildFromPostfix(const string& postfix) {
+bool BET<T>::buildFromPostfix(const std::string& postfix) {
+    
+    // if string is empty, return false
+    if (postfix.size() == 0) {
+        std::cout << "Postfix expression is invalid." << std::endl;
+        root = nullptr;
+        return false;
+    }
+    
+    // prep tokenized string using stringstream
+    std::string token;
+    std::stringstream ss(postfix);
 
+    // create stack to push nodes to while constructing 
+    std::stack<BinaryNode*> t_stack;
+
+    // clear in case tree is not empty at time of calling
+    makeEmpty(root);
+
+    // while sstream isn't empty, push new string to token
+    while (ss >> token) {
+        // create node
+        BinaryNode* node = new BinaryNode(token);
+
+        if (isOperand(token)) {
+            t_stack.push(node);
+        }
+        else if (isOperator(token) && t_stack.size() < 2) {
+            std::cout << "Postfix expression is invalid." << std::endl;
+            root = nullptr;
+            return false;
+        }
+        else {
+            node->left = t_stack.top();
+            t_stack.pop();
+            node->right = t_stack.top();
+            t_stack.pop();
+            t_stack.push(node);
+        }
+    }
+
+    if ((t_stack.size() == 1)){
+        root = t_stack.top();
+        t_stack.pop();
+    }
+    else {
+        std::cout << "Postfix expression is invalid." << std::endl;
+        return false;
+    }
+
+    return true;
 }
 
 template <typename T>
-const BET& BET<T>::operator=(const BET& tree) {
+// copy operator
+const BET<T>& BET<T>::operator=(const BET& tree) {
     // clear base tree, clone other tree
     makeEmpty(root);
     root = clone(tree.root);
+    return *this;
 }
 
 template <typename T>
 void BET<T>::printInfixExpression() {
-
+  printInfixExpression(root);
+  std::cout << std::endl;
 }
 
 template <typename T>
 void BET<T>::printPostfixExpression() {
-
+  printPostfixExpression(root);
+  std::cout << std::endl;
 }
 
 /* Getter Functions */
@@ -77,16 +136,67 @@ bool BET<T>::empty() {
     return true;
 }
 
-
 /* Helper Functions */
 template <typename T>
 void BET<T>::printInfixExpression(BinaryNode *n) {
+    bool isParen = false;
 
+    // inorder transversal of tree to get infix notation
+    if (n == nullptr) {
+        // if nothing in (sub)tree, return immediately
+        return;
+    }
+    else if (n->left == nullptr && n->right == nullptr) {
+        // if size == 1, print current element
+        std::cout << n->element << " ";
+    }
+    else {
+        // check to see if parenthesis are needed
+        if (isOperator(n->element) && isOperator(n->right->element)) {
+            if (precedence(n->element) >= precedence(n->right->element)) {
+                if (precedence(n->element) == 1) {
+                    std::cout << "( ";
+                    isParen = true;
+                }
+            }
+        }
+        
+        // print down right subtree
+        printInfixExpression(n->right);
+
+        // print matching parenthesis then reset bool
+        if (isParen) {
+            std::cout << ") ";
+        }
+        isParen = false;
+
+        // print out root
+        std::cout << n->element << " ";
+
+        // check to see if parenthesis are needed
+        if (isOperator(n->element) && isOperator(n->left->element)) {
+            if (precedence(n->element) >= precedence(n->left->element)) {
+                if (precedence(n->element) == 1) {
+                    std::cout << "( ";
+                    isParen = true;
+                }
+            }
+        }
+
+        // print down left subtree
+        printInfixExpression(n->left);
+
+        // print matching parenthesis
+        if (isParen) {
+            std::cout << ") ";
+        }
+    }
 }
 
 template <typename T>
 void BET<T>::makeEmpty(BinaryNode* &t) {
     if (t != nullptr) {
+        // try to delete left/right subtrees and then delete this node
         makeEmpty(t->left);
         makeEmpty(t->right);
         delete t;
@@ -94,17 +204,30 @@ void BET<T>::makeEmpty(BinaryNode* &t) {
 }
 
 template <typename T>
-BinaryNode* BET<T>::clone(BinaryNode *t) {
-    // create new node down T
+typename BET<T>::BinaryNode* BET<T>::clone(BinaryNode *t) {
+    // create new node down t
     if (t != nullptr) {
-        return new BinaryNode(t->data, clone(t->left), clone(t->right));
+        return new BinaryNode(t->element, clone(t->left), clone(t->right));
     }
     return nullptr;
 }
 
 template <typename T>
 void BET<T>::printPostfixExpression(BinaryNode *n) {
-
+    if (n == nullptr) {
+        // if nothing in (sub)tree, return immediately
+        return;
+    }
+    else if (n->left == nullptr && n->right == nullptr) {
+        // if size == 1, print current element
+        std::cout << n->element << std::endl;
+    }
+    else {
+        // run down nodes left and right, then print this node
+        printPostfixEspression(n->right);
+        printPostfixExpression(n->left);
+        std::cout << n->element << " ";
+    }
 }
 
 template <typename T>
@@ -114,6 +237,7 @@ size_t BET<T>::size(BinaryNode *t) {
         return 0;
     }
     else {
+        // count this node, then check its left/right nodes
         size_t s = 1;
         s += size(t->left);
         s += size(t->right);
@@ -135,4 +259,36 @@ size_t BET<T>::leaf_nodes(BinaryNode *t) {
         }
     }
     return 0;
+}
+
+// check if passed string is an operator
+bool isOperator(std::string s) {
+    if (s == "*" || s == "/" || s == "+" || s == "-") {
+        return true;
+    }
+    return false;
+}
+
+// check if passed string is an operand
+bool isOperand(std::string s){
+    for (char ch : s) {
+        if (isalnum(ch)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+// check precedence of passed string
+int precedence(std::string s) {
+    if (s == "(" || s == ")") {
+        return 2;
+    }
+    else if (s == "*" || s == "/") {
+        return 1;
+    }
+    else if (s == "+" || s == "-") {
+        return 0;
+    }
+    return -1;
 }
